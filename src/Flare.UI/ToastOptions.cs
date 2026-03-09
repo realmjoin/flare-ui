5670210f362fbe39a58eb8a4b8aa77b990ceb072
Jan Berdel <10817+Jabe@users.noreply.github.com>
2026-03-09 20:11:11 +0100

    Add conditional rendering for close button in FlareToast component

--- src/Flare.UI/ToastOptions.cs
+++ src/Flare.UI/ToastOptions.cs
@@ -7,5 +7,6 @@ public sealed class ToastOptions
     public bool ShowProgress { get; set; } = true;
     public bool PauseOnHover { get; set; } = true;
     public bool Persistent { get; set; }
+    public bool ShowCloseButton { get; set; } = true;
     public Func<Task>? OnClick { get; set; }
 }
